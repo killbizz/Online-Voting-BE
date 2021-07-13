726a23fb826e0148142607296be855b27e1ce633
killbizz <45452791+killbizz@users.noreply.github.com>
2021-07-13 19:19:19 +0200

    fix configuration

--- src/main/java/com/onlinevoting/OnlineVoting/OnlineVotingApplication.java
+++ src/main/java/com/onlinevoting/OnlineVoting/OnlineVotingApplication.java
@@ -1,18 +1,22 @@
 package com.onlinevoting.OnlineVoting;
 
+import com.onlinevoting.OnlineVoting.model.User;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
-public class OnlineVotingApplication {
+public class OnlineVotingApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineVotingApplication.class, args);
 	}
 
-	// @Override
-    // public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-    //     config.exposeIdsFor(Synthesizer.class);
-	// 	config.exposeIdsFor(User.class);
-    // }
+	@Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(User.class);
+    }
 }
